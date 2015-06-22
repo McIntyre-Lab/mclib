dce3b81adcb9345af1c326e7e29594a90dd88a57
Justin Fear <jfear@ufl.edu>
2015-06-22 11:21:57 -0400

    Moves orderDF to munging and extends the mergeAndDrop to allow drop and keep flags.

--- munging.py
+++ munging.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-""" A set of common helper function to getting things done quickly and cleanly """
+""" A set of data munging functions to getting things done quickly and cleanly """
 
 
 def asList(x):
@@ -20,11 +20,10 @@ def asList(x):
         raise ValueError('Could not convert {} to a list'.format(str(x)))
 
 
-def mergeAndDrop(df, flags, left_on, right_on, flagName, keep_in_list=None, keep_logic=None, all=True):
+def mergeAndDrop(df, flags, left_on, right_on, flagName, keep_in_list=None, keep_logic=None, drop_in_list=None, drop_logic=None, all=True):
     """ Merge on a set of flags and drop depending on a criteria.
 
     Arguments:
-
         :type df: pandas.DataFrame
         :param df: A data frame that you want to merge flags on to.
 
@@ -35,24 +34,31 @@ def mergeAndDrop(df, flags, left_on, right_on, flagName, keep_in_list=None, keep
 
         :param str right_on: Column name that you want to merge in your flags data frame.
 
-        :param list flagName: Column name with flags.
+        :param list flagName: Column names with flags.
 
         :param list keep_in_list: List of what flag values to keep.
 
         :param str keep_logic: A string with the logical opperator for which
             flags to keep. For example, ('== 0', '<= 1', '>= 10', '!= 2').
 
+        :param list drop_in_list: List of what flag values to drop.
+
+        :param str drop_logic: A string with the logical opperator for which
+            flags to drop. For example, ('== 0', '<= 1', '>= 10', '!= 2').
+
         :param bool all: If True then a row will be kept if all flags return
-            True. If false, a row will be kept if any of the values were True.
+            True (keep) or False (drop). If false, a row will be kept if any of the
+            values were True (keep) or any of the values were False (drop).
 
     Returns:
-
-        :return: A data frame without the rows that were flagged
         :rtype: pd.DataFrame
+        :return: A data frame with the rows that were flagged (keep) or without
+            the flagged rows (drop).
 
     """
 
     # Merge datasets
+    ## Re-index first to make merging easier.
     dfI = df.reset_index()
     flagsI = flags.reset_index()
     merged = dfI.merge(flagsI, how='left', left_on=left_on, right_on=right_on)
@@ -61,8 +67,11 @@ def mergeAndDrop(df, flags, left_on, right_on, flagName, keep_in_list=None, keep
     nameList = asList(flagName)
 
     # Drop Flags
-    if keep_in_list and keep_logic:
-        print "keep_in_list and keep_logic are mutually exclusive. Please only provide one."
+    if sum([keep_in_list, keep_logic, drop_in_list, drop_logic]) > 1:
+        print "keep_in_list, keep_logic, drop_in_list, drop_logic are mutually exclusive. Please only provide one."
+        raise Exception
+    elif sum([keep_in_list, keep_logic, drop_in_list, drop_logic]) == 0:
+        print "Please provide at least on of keep_in_list, keep_logic, drop_in_list, drop_logic."
         raise Exception
 
     if keep_in_list:
@@ -74,12 +83,22 @@ def mergeAndDrop(df, flags, left_on, right_on, flagName, keep_in_list=None, keep
         # Create mask where drop_when values are False
         cleanMask = merged[nameList].isin(flagList).values
 
-    elif keep_logic:
-            cleanMask = eval('merged[nameList] ' + keep_logic).values
+    elif drop_in_list:
+        print "Dropping flags in given list."
 
-    else:
-        print "Please provide either a keep_in_list and keep_logic."
-        raise Exception
+        # Make sure keep_in_list is a list
+        flagList = asList(drop_in_list)
+
+        # Create mask where drop_when values are False
+        cleanMask = ~merged[nameList].isin(flagList).values
+
+    elif keep_logic:
+        print "Keeping flags with {}.".format(keep_logic)
+        cleanMask = eval('merged[nameList] ' + keep_logic).values
+
+    elif drop_logic:
+        print "Dropping flags with {}.".format(drop_logic)
+        cleanMask = eval('~merged[nameList] ' + keep_logic).values
 
     # Return df with specific rows
     if all:
@@ -87,6 +106,30 @@ def mergeAndDrop(df, flags, left_on, right_on, flagName, keep_in_list=None, keep
     else:
         return df[cleanMask.any(axis=1)]
 
+
+def orderDf(df, varList):
+    """ Re-order the columns of a dataframe.
+
+    Arguments:
+        :type df: pandas.DataFrame
+        :param df: A pandas dataframe
+
+        :param list varList: List of column names you want to be placed at the
+            front of your data frame.
+
+    Returns:
+        :rtype: pandas.DataFrame
+        :returns: A pandas DataFrame with re-ordered columns.
+
+    """
+    # Create a list of the other columns in df, not including the columns that
+    # are being moved.
+    otherCols = [x for x in df.columns if x not in varList]
+
+    # Create new data frame with the columns name in varList at the front.
+    dfOrder = df[varList + otherCols].copy()
+
+    return dfOrder
 
 if __name__ == '__main__':
     pass
